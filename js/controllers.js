837e033915e0e803cc9f1538df4940082289245f
wohlig <chintan@wohlig.com>
2014-11-07 18:35:11 +0530

    changes for Auto City Selection
    changes for Auto City Selection

--- js/controllers.js
+++ js/controllers.js
@@ -1,9 +1,9 @@
-var lat=0;
-var long=0;
+var lat = 0;
+var long = 0;
 var phonecatControllers = angular.module('phonecatControllers', ['templateservicemod', 'restservice', 'ngRoute']);
 
 phonecatControllers.controller('home',
-  function ($scope, TemplateService, RestService, $location, $sce) {
+    function ($scope, TemplateService, RestService, $location, $sce) {
         $scope.template = TemplateService;
         TemplateService.header = "views/header.html";
         TemplateService.navigation = "views/navigation.html";
@@ -11,130 +11,153 @@ phonecatControllers.controller('home',
         TemplateService.content = "views/content.html";
         TemplateService.footer = "views/footer.html";
         TemplateService.footerbottom = "views/footerbottom.html";
-        $scope.searchshow=false;
-        $scope.searchid="";
-        $scope.form=[];
-        $scope.form.cityy="9";
-        $scope.homecategory={};
-    //  get area from city
-    
-        var maincategories = function (data, status){
-            console.log(data[0].logo);
-            console.log("formated data");
-            $scope.homecategory=data;
-            for(var i=0 ; i < data.length ; i++)
-            {
-                $scope.homecategory[i].logo= $sce.trustAsHtml(data[i].logo);
-            }
-            
-            $scope.homecategory=partitionarray($scope.homecategory,6);
-        };
-    
-        RestService.getallparentcategories().success(maincategories);
-        
-        var getlocation = function (data, status){
-            console.log(data);
-            $scope.areas=data;
-            $scope.form.area=data[0].id;
-        };
-    
-        $scope.citychange = function (city){
-            console.log(city);
-            RestService.viewonecitylocations(city).success(getlocation);
-        };
-        $scope.totextbox = function (name,id){
-            $("input[name=abc]").val(name);
-            $scope.searchid=id;
-            $scope.searchshow=false; 
-        }
-        
-        $scope.innershearch = function (){
-            $location.url("/subcategory/"+$scope.searchid);
-        };
-        
-        var searchsuccess = function(data, status){
-            console.log(data);
-            if(data!="")
-            {
-                $scope.searchdrop=data;
-                $scope.searchshow=true;
-            }else{
-                $scope.searchshow=false;    
-                
-            }
-        };
-        $scope.searchlist = function(text){
-            console.log(text);
-            if(text!="")
-            {
-                RestService.searchcategory(text).success(searchsuccess);
-            }else{
-                $scope.searchshow=false;
-            }
-        }
-    
-        var getcity = function (data, status){
-            console.log(data);
-            $scope.cities=data;
-        };
-        RestService.getallcity().success(getcity);
-      // searching 
-      
-        var searchdata = function (data, status) {
-            console.log("in home");
-            console.log(data);
-        };
-        RestService.searchcategory("h").success(searchdata);
-      
-      
-        
-      // location lat long
-        
-        $scope.coords = {};
-      
-      //Google Maps API Lat Long
-
+        $scope.searchshow = false;
+        $scope.searchid = "";
+        $scope.form = [];
+        //$scope.form.cityy = 9;
+        $scope.homecategory = {};
+        //  get area from city
 
         function showPosition2(position) {
             var latlon = position.coords.latitude + "," + position.coords.longitude;
             console.log("Positions:.........");
             console.log(position.coords);
-            $scope.coords = position.coords;
+            //$scope.coords = position.coords;
             lat = position.coords.latitude;
             long = position.coords.longitude;
-            console.log("my distance in km");
-            console.log(getDistance(15,12,lat,long));
+            $scope.cityis = {};
+            $.get("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + long + "&key=AIzaSyDqN3t8_Nb04MF7jTufq-bkEHogZxyeUHY", {}, function (data) {
+                console.log(data);
+                data = data.results[0].address_components;
+                for (var i = 0; i < data.length; i++) {
+                    if (data[i].types[0] == "locality") {
+                        $scope.cityis.selected = data[i].long_name;
+                        console.log($scope.cityis.selected);
+                    }
+                }
+                var citywegot = 9;
+                var cities = $scope.cities;
+                for (var i = 0; i < $scope.cities.length; i++) {
+                    
+                    console.log($scope.cities[i].name == $scope.cityis.selected);
+                    if ($scope.cities[i].name == $scope.cityis.selected) {
+                    
+                        citywegot = $scope.cities[i].id;
+                    }
+                }
+                $scope.form.cityy = citywegot;
+                $scope.$apply();
+                console.log(citywegot);
+                $scope.citychange(citywegot);
+                $scope.$apply();
+
+            });
+
         }
+
+        var maincategories = function (data, status) {
+            console.log(data[0].logo);
+            console.log("formated data");
+            $scope.homecategory = data;
+            for (var i = 0; i < data.length; i++) {
+                $scope.homecategory[i].logo = $sce.trustAsHtml(data[i].logo);
+            }
+
+            $scope.homecategory = partitionarray($scope.homecategory, 6);
+        };
+
+        RestService.getallparentcategories().success(maincategories);
+
+        var getlocation = function (data, status) {
+            console.log(data);
+            $scope.areas = data;
+            $scope.form.area = data[0].id;
+        };
+
+        $scope.citychange = function (city) {
+            console.log(city);
+            RestService.viewonecitylocations(city).success(getlocation);
+        };
+        $scope.totextbox = function (name, id) {
+            $("input[name=abc]").val(name);
+            $scope.searchid = id;
+            $scope.searchshow = false;
+        }
+
+        $scope.innershearch = function () {
+            $location.url("/subcategory/" + $scope.searchid);
+        };
+
+        var searchsuccess = function (data, status) {
+            console.log(data);
+            if (data != "") {
+                $scope.searchdrop = data;
+                $scope.searchshow = true;
+            } else {
+                $scope.searchshow = false;
+
+            }
+        };
+        $scope.searchlist = function (text) {
+            console.log(text);
+            if (text != "") {
+                RestService.searchcategory(text).success(searchsuccess);
+            } else {
+                $scope.searchshow = false;
+            }
+        }
+
+        var getcity = function (data, status) {
+            console.log(data);
+            $scope.cities = data;
+        };
+        RestService.getallcity().success(getcity);
+        // searching 
+
+        var searchdata = function (data, status) {
+            console.log("in home");
+            console.log(data);
+        };
+        RestService.searchcategory("h").success(searchdata);
+
+
+
+        // location lat long
+
+        $scope.coords = {};
+
+        //Google Maps API Lat Long
+
+
+
 
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(showPosition2, showError);
         } else {
             x.innerHTML = "Geolocation is not supported by this browser.";
         }
-    
-    
-    //function for home category
-    
-    
-        function partitionarray(myarray,number) {
-            var arrlength=myarray.length;
-            var newarray=[];
-            var j=-1;
-            for(var i=0;i<arrlength;i++)
-            {
-                if(i%number==0)
-                {
+
+
+        //function for home category
+
+
+        function partitionarray(myarray, number) {
+            var arrlength = myarray.length;
+            var newarray = [];
+            var j = -1;
+            for (var i = 0; i < arrlength; i++) {
+                if (i % number == 0) {
                     j++;
-                    newarray[j]=[];
+                    newarray[j] = [];
                 }
                 newarray[j].push(myarray[i]);
             }
             return newarray;
         };
-    
-    
 
-  });
+
+
+    });
 
 phonecatControllers.controller('category',
     function ($scope, TemplateService, RestService, $location, $routeParams) {
@@ -146,7 +169,7 @@ phonecatControllers.controller('category',
         $scope.msg = "";
         $scope.msgarea = false;
         //listiung by category id
-        
+
         var getlisting = function (data, status) {
             console.log(data);
             if (data == "") {
@@ -159,20 +182,20 @@ phonecatControllers.controller('category',
 
         };
         RestService.getlistingbycategory($routeParams.id).success(getlisting);
-    
-    
-    //filter
-        var getfilter = function (data, status){
+
+
+        //filter
+        var getfilter = function (data, status) {
             console.log(data);
-            $scope.filters=data;
+            $scope.filters = data;
         };
         RestService.getfilter($routeParams.id).success(getfilter);
-    
-    //filter to category listing
-    $scope.one1="";
-        $scope.filterselect = function(iid,name){
-            $scope.one1=name;
-            $scope.one1="filterselected";
+
+        //filter to category listing
+        $scope.one1 = "";
+        $scope.filterselect = function (iid, name) {
+            $scope.one1 = name;
+            $scope.one1 = "filterselected";
             $scope.listings = "";
             RestService.getlistingbycategory(iid).success(getlisting);
         }
@@ -223,40 +246,39 @@ phonecatControllers.controller('detail',
     });
 
 phonecatControllers.controller('about',
-  function ($scope, TemplateService) {
+    function ($scope, TemplateService) {
         $scope.template = TemplateService;
         $scope.menutitle = NavigationService.makeactive("About");
         TemplateService.title = $scope.menutitle;
         $scope.navigation = NavigationService.getnav();
-  });
+    });
 
 phonecatControllers.controller('login',
-  function ($scope, TemplateService, RestService, $location, $routeParams) {
+    function ($scope, TemplateService, RestService, $location, $routeParams) {
         $scope.template = TemplateService;
         TemplateService.content = "views/login.html";
         TemplateService.slider = false;
         TemplateService.navigation = "views/innerheader.html";
-        $scope.signupmsg="";
-        $scope.signupmsgg=false;
-        $scope.clickme=function(){
+        $scope.signupmsg = "";
+        $scope.signupmsgg = false;
+        $scope.clickme = function () {
             console.log("helloooooo");
         }
-        var getuser = function (data, status){
+        var getuser = function (data, status) {
             console.log(data);
-            if(data==false)
-            {
-        $scope.signupmsgg=true;
-            
-              $scope.signupmsg="Already Exist. Choose Another Id";  
+            if (data == false) {
+                $scope.signupmsgg = true;
+
+                $scope.signupmsg = "Already Exist. Choose Another Id";
             }
         };
-        $scope.signupuser = function (signup){
+        $scope.signupuser = function (signup) {
             console.log(signup);
-            RestService.signup(signup.email,signup.password).success(getuser);
+            RestService.signup(signup.email, signup.password).success(getuser);
         }
-    
+
         //signup
-  });
+    });
 phonecatControllers.controller('portfolio', ['$scope', 'TemplateService',
   function ($scope, TemplateService) {
         $scope.template = TemplateService;
