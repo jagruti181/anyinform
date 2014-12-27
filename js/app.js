b9df666d7b70d0d6478cfea00d70ffa16beb1ac2
jagruti181 <patiljagruti181@gmail.com>
2014-12-27 15:09:39 +0530

    path

--- js/app.js
+++ js/app.js
@@ -77,11 +77,11 @@ firstapp.filter('imagepath', function () {
 firstapp.filter('imagepath2', function () {
     return function (input) {
         if (input == "") {
-            return "http://localhost/anyinform/assets/img/default.jpg";
-//                        return "http://localhost/foranyinformation/assets/img/default.jpg";
+            return "http://mafiawarloots.com/anyinform/assets/img/default.jpg";
+//          return "http://localhost/foranyinformation/assets/img/default.jpg";
         } else {
-            return "http://localhost/anyinform/lib/images/" + input;
-//            return "http://localhost/foranyinformation/uploads/" + input;
+            return "http://mafiawarloots.com/anyinform/lib/images/" + input;
+//          return "http://localhost/foranyinformation/uploads/" + input;
         }
     };
 });
@@ -89,11 +89,11 @@ firstapp.filter('imagepath2', function () {
 firstapp.filter('imagepath1', function () {
     return function (input) {
         if (input == "") {
-//            return "http://mafiawarloots.com/foranyinformation/assets/img/default.jpg";
-            return "http://localhost/foranyinformation/assets/img/default.jpg";
+            return "http://mafiawarloots.com/foranyinformation/assets/img/default.jpg";
+//            return "http://localhost/foranyinformation/assets/img/default.jpg";
         } else {
-//            return "http://mafiawarloots.com/foranyinformation/lib/images/" + input;
-            return "http://localhost/foranyinformation/lib/images/" + input;
+            return "http://mafiawarloots.com/foranyinformation/lib/images/" + input;
+//            return "http://localhost/foranyinformation/lib/images/" + input;
         }
     };
 });
